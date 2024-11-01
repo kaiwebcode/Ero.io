44c6e33660a63ef23b43e589fa6aa8339baef6eb
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-11-01 20:53:34 +0530

    Deployment

--- app/(routes)/dashboard/_components/SideNavBottomSection.tsx
+++ app/(routes)/dashboard/_components/SideNavBottomSection.tsx
@@ -39,7 +39,7 @@ function SideNavBottomSection({ onFileCreate, totalFiles }: any) {
   ];
   const [fileInput, setFileInput] = useState("");
   return (
-    <div className="mt-72">
+    <div className="mt-64">
       {menuList.map((menu, index) => (
         <h2
           key={index}
